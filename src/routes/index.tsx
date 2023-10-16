1b17e0bd59d03f363ebae54e376227d2991c5d76
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-10-16 14:18:14 +0200

    chore: :construction_worker: Fix typescript errors

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
 
 import { HomeScreen } from '../screens/Home';
 import { WTRScreen } from '../screens/WTR';
