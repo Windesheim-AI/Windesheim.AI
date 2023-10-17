a266074831f0034d382d547fe2e76d2000c9da68
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-10-17 09:34:12 +0200

    bug: :ambulance: Fix type clashing between Jest and Cypres

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     preset: '@testing-library/react-native',
+    roots: ['<rootDir>/__tests__'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
     setupFilesAfterEnv: ['./jest-setup.ts'],
     transformIgnorePatterns: [
