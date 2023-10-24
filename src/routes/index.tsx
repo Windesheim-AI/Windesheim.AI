eae80072e477a98812315088a3e203e75d8821d1
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-10-24 15:28:12 +0200

    bug: :ambulance: Fix recursion in route usage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,24 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 
-import { Routes, screens } from './routes';
+import { Routes } from './routes';
+import { HomeScreen } from '../screens/Home';
+import { DataScreen } from '../screens/Settings/Data';
+import { LanguageScreen } from '../screens/Settings/Language';
+import { SettingsScreen } from '../screens/Settings/Settings';
+import { TestScreen } from '../screens/Test';
+import { WTRScreen } from '../screens/WTR';
 
 const Stack = createNativeStackNavigator();
+
+const screens = [
+    { name: Routes.Home, component: HomeScreen },
+    { name: Routes.Test, component: TestScreen },
+    { name: Routes.WindesheimTechRadar, component: WTRScreen },
+    { name: Routes.Settings, component: SettingsScreen },
+    { name: Routes.DataSettings, component: DataScreen },
+    { name: Routes.LanguageSettings, component: LanguageScreen },
+];
 
 export const Router = () => {
     return (
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,3 @@
-import { HomeScreen } from '../screens/Home';
-import { DataScreen } from '../screens/Settings/Data';
-import { LanguageScreen } from '../screens/Settings/Language';
-import { SettingsScreen } from '../screens/Settings/Settings';
-import { TestScreen } from '../screens/Test';
-import { WTRScreen } from '../screens/WTR';
-
 export enum Routes {
     Home = 'Home',
     Test = 'Test',
@@ -13,12 +6,3 @@ export enum Routes {
     DataSettings = 'DataSettings',
     LanguageSettings = 'LanguageSettings',
 }
-
-export const screens = [
-    { name: Routes.Home, component: HomeScreen },
-    { name: Routes.Test, component: TestScreen },
-    { name: Routes.WindesheimTechRadar, component: WTRScreen },
-    { name: Routes.Settings, component: SettingsScreen },
-    { name: Routes.DataSettings, component: DataScreen },
-    { name: Routes.LanguageSettings, component: LanguageScreen },
-];
