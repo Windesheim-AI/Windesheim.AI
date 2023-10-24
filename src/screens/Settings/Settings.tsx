a2305ff4a039c1317b5ec8c7ec75f500f98d72fd
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-10-24 11:20:31 +0200

    chore: :construction: Theme settings screen

--- src/screens/Settings/Settings.tsx
+++ src/screens/Settings/Settings.tsx
@@ -1,21 +1,24 @@
 import * as React from 'react';
 
+import { SettingButton } from '../../components/buttons/SettingButton';
 import { SettingCard } from '../../components/card/SettingCard';
 import { PageView } from '../../components/general/PageView';
 import { ThemeSwitcher } from '../../components/settings/ThemeSwitcher';
+import { Routes } from '../../routes/routes';
 
 export const SettingsScreen = () => {
     return (
         <PageView title="Settings">
+            <SettingCard icon="cog" title="Enable dark mode">
+                <ThemeSwitcher />
+            </SettingCard>
+
             <SettingButton
                 icon="globe"
                 title="Language"
                 description="Change the language that the app uses."
                 screenName={Routes.LanguageSettings}
             />
-            <SettingCard icon="cog" title="Enable dark mode">
-                <ThemeSwitcher />
-            </SettingCard>
         </PageView>
     );
 };
