2076cef7a572835c870477ad3b568c401ea741ce
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-10-24 16:45:37 +0200

    feat: :sparkles: Use the color scheme based on the preference of the user

--- src/redux/slices/ThemeSlice.ts
+++ src/redux/slices/ThemeSlice.ts
@@ -1,11 +1,12 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Appearance } from 'react-native';
 
 export interface ThemeState {
     theme: 'light' | 'dark';
 }
 
 const initialState: ThemeState = {
-    theme: 'light',
+    theme: Appearance.getColorScheme() ?? 'light',
 };
 
 export const themeSlice = createSlice({
