3063d22d28846ba6ac28e61e4e05b4b1561a6ddf
Rick Koekkoek <rickkoekkoek@outlook.com>
2023-10-24 08:08:10 +0200

    test: :white_check_mark: Added Component Test to GoBackButton

--- src/components/buttons/GoBackButton.tsx
+++ src/components/buttons/GoBackButton.tsx
@@ -1,4 +1,3 @@
-import { useFonts, Inter_500Medium } from '@expo-google-fonts/inter';
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
@@ -15,10 +14,6 @@ export const GoBackButton = ({
     onPress,
     buttonText = 'Go Back',
 }: GoBackButtonProps) => {
-    const [fontsLoaded, fontError] = useFonts({
-        Inter_500Medium,
-    });
-
     const colors = useColorConfig();
     const navigation = useNavigation();
 
@@ -26,10 +21,6 @@ export const GoBackButton = ({
         onPress = () => {
             navigation.goBack();
         };
-    }
-
-    if (!fontsLoaded && !fontError) {
-        return null;
     }
 
     const styles = StyleSheet.create({
@@ -42,7 +33,6 @@ export const GoBackButton = ({
         },
         buttonText: {
             color: colors.text,
-            fontFamily: 'Inter_500Medium',
             fontSize: 16,
             fontWeight: 'bold',
             marginLeft: 8,
