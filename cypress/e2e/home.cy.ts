b35d1f8617f011afd227f9044230fa5bb455662f
LeeMineo <lms4882@gmail.com>
2023-10-25 14:17:36 +0200

    Fixed Error10

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -6,5 +6,6 @@ describe('Home page tests', () => {
     });
     it('test if element exists', () => {
         cy.visit('/');
+        cy.get('.r-WebkitOverflowScrolling-150rngu > .r-flexDirection-18u37iz > :nth-child(1) > .css-view-175oi2r').contains('News1');
     })
 });
