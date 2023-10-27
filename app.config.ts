7896dd8e8111fa0315acfeaa1edca53eb638adda
Rick Koekkoek <rickkoekkoek@outlook.com>
2023-10-27 12:56:04 +0200

    feat: :art: Added Logo, Splash Screen, Favicon to the app

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,13 @@
+import { ExpoConfig } from '@expo/config';
+
 export type AppConfigOptions = {
     splashScreenTime: number;
 };
 
 export const appConfig: AppConfigOptions = {
     splashScreenTime: 500,
+};
+
+export default ({ config }: { config: ExpoConfig }) => {
+    return config;
 };
