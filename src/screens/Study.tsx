0ea6f742685a63e26d85b3ba2bfdd0d053392c3a
Rick Koekkoek <rickkoekkoek@outlook.com>
2023-11-06 14:00:11 +0100

    fix: :bug: removed staticloading from study

--- src/screens/Study.tsx
+++ src/screens/Study.tsx
@@ -2,11 +2,9 @@ import * as React from 'react';
 
 import { ListButton } from '../components/buttons/ListButton';
 import { PageView } from '../components/general/PageView';
-import { useStaticLoading } from '../lib/utility/loaderFunctions';
 import { Routes } from '../routes/routes';
 
 export const StudyScreen = () => {
-    useStaticLoading();
     return (
         <PageView title="Welcome to WIN game">
             <ListButton
