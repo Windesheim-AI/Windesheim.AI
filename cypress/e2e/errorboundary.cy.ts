3acd5ddb1d3a049416e344e18d8db2b6ebc0ffd1
LeeMineo <lms4882@gmail.com>
2023-11-10 11:13:33 +0100

    Feat/US-9_ClearValidationMes

---
+++ cypress/e2e/errorboundary.cy.ts
@@ -0,0 +1,6 @@
+describe('Errorboundary', () => {
+    it('the Errorboundary page works', () => {
+       cy.visit('/src/components/error/ErrorBoundary.tsx');
+    });
+ });
+ 
---
+++ src/components/error/ErrorBoundary.tsx
@@ -0,0 +1,119 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable react-native/no-color-literals */
+import Constants from 'expo-constants';
+import LottieView from 'lottie-react-native';
+import * as React from 'react';
+import {
+    Text,
+    View,
+    TouchableOpacity,
+    StyleSheet,
+    Animated,
+    Dimensions,
+} from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: Constants.statusBarHeight,
+        padding: 8,
+        textAlign: 'center',
+        backgroundColor: 'white',
+    },
+    title: {
+        fontSize: windowWidth * 0.08,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    textContainer: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: windowWidth * 0.1,
+    },
+    text: {
+        marginVertical: windowWidth * 0.04,
+        fontSize: windowWidth * 0.05,
+        padding: 8,
+    },
+    button: {
+        justifyContent: 'center', // 세로 정렬 추가
+        alignItems: 'center', // 가로 정렬 추가
+        width: windowWidth * 0.3,
+        height: windowHeight * 0.08,
+        backgroundColor: 'blue',
+        borderRadius: 8,
+    },
+    buttonText: {
+        fontSize: windowWidth * 0.05,
+        color: 'white',
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+    animationContainer: {
+        width: windowWidth * 0.8,
+        height: windowWidth * 0.8,
+    },
+});
+
+interface ErrorFallbackProps {
+    error: Error;
+    resetError: () => void;
+}
+
+const ErrorFallback: React.FC<ErrorFallbackProps> = ({
+    error,
+    resetError,
+}: ErrorFallbackProps) => {
+    const scaleValue = React.useRef(new Animated.Value(1)).current;
+
+    const handlePress = () => {
+        Animated.sequence([
+            Animated.timing(scaleValue, {
+                toValue: 1.2,
+                duration: 100,
+                useNativeDriver: true,
+            }),
+            Animated.timing(scaleValue, {
+                toValue: 1,
+                duration: 100,
+                useNativeDriver: true,
+            }),
+        ]).start(() => {
+            resetError();
+        });
+    };
+
+    return (
+        <View style={styles.container} testID="error-fallback-container">
+            <View style={styles.animationContainer}>
+                <LottieView
+                    source={require('../../assets/json/500_man.json')}
+                    autoPlay
+                    loop
+                    style={{ width: '100%', height: '100%' }}
+                />
+            </View>
+            <Text style={styles.title}>Something happened!</Text>
+            <Text style={styles.text}>{error.toString()}</Text>
+            <View style={styles.textContainer}>
+                <TouchableOpacity
+                    style={[
+                        styles.button,
+                        { transform: [{ scale: scaleValue }] },
+                    ]}
+                    onPress={handlePress}
+                >
+                    <Text style={styles.buttonText}>Try again</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+
+export default ErrorFallback;
