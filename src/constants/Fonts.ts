13e1131cf491ca5a4bae49444969eacb9f1a35c0
Yannick Bruintjes <yannickbruintjes@hotmail.com>
2023-11-20 16:51:40 +0100

    refactor: changed so build will work

--- src/constants/Fonts.ts
+++ src/constants/Fonts.ts
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import * as Font from 'expo-font';
-import { useEffect, useState } from 'react';
+import { useFonts as useExpoFonts } from '@expo-google-fonts/inter';
 import { TextStyle } from 'react-native';
 
 import { useColorConfig } from './Colors';
 import { useAppSelector } from '../redux/Hooks';
+
+// import the font files from the assets folder
 
 export const MaxSize = 40;
 export const MinSize = 12;
@@ -42,36 +43,24 @@ type TextStyling = {
 export function useFonts() {
     const colors = useColorConfig();
 
-    const [fontsLoaded, setFontsLoaded] = useState(false);
-    async function loadInFonts() {
-        await Font.loadAsync({
-            Inter_100Thin: require('../assets/fonts/Inter-Thin.ttf'),
-            Inter_200ExtraLight: require('../assets/fonts/Inter-ExtraLight.ttf'),
-            Inter_300Light: require('../assets/fonts/Inter-Light.ttf'),
-            Inter_400Regular: require('../assets/fonts/Inter-Regular.ttf'),
-            Inter_500Medium: require('../assets/fonts/Inter-Medium.ttf'),
-            Inter_600SemiBold: require('../assets/fonts/Inter-SemiBold.ttf'),
-            Inter_700Bold: require('../assets/fonts/Inter-Bold.ttf'),
-            Inter_800ExtraBold: require('../assets/fonts/Inter-ExtraBold.ttf'),
-            Inter_900Black: require('../assets/fonts/Inter-Black.ttf'),
-        });
-    }
-    useEffect(() => {
-        async function loadFonts() {
-            await loadInFonts();
-            setFontsLoaded(true);
-        }
-
-        // eslint-disable-next-line no-void
-        void loadFonts();
-    }, []);
+    const [areFontsLoaded] = useExpoFonts({
+        Inter_100Thin: require('../assets/fonts/Inter-Thin.ttf'),
+        Inter_200ExtraLight: require('../assets/fonts/Inter-ExtraLight.ttf'),
+        Inter_300Light: require('../assets/fonts/Inter-Light.ttf'),
+        Inter_400Regular: require('../assets/fonts/Inter-Regular.ttf'),
+        Inter_500Medium: require('../assets/fonts/Inter-Medium.ttf'),
+        Inter_600SemiBold: require('../assets/fonts/Inter-SemiBold.ttf'),
+        Inter_700Bold: require('../assets/fonts/Inter-Bold.ttf'),
+        Inter_800ExtraBold: require('../assets/fonts/Inter-ExtraBold.ttf'),
+        Inter_900Black: require('../assets/fonts/Inter-Black.ttf'),
+    });
 
     const fontMap: TextStyling = {
         h1: {
             fontSize: 24,
             fontWeight: 'bold',
             color: colors.titleDefault,
-            fontFamily: fontsLoaded ? 'Inter_600SemiBold' : 'System',
+            fontFamily: areFontsLoaded ? 'Inter_600SemiBold' : 'System',
         },
         h2: {
             fontSize: 18,
@@ -84,7 +73,7 @@ export function useFonts() {
         h4: {
             fontSize: 14,
             color: colors.text,
-            fontFamily: fontsLoaded ? 'Inter_400Regular' : 'System',
+            fontFamily: areFontsLoaded ? 'Inter_400Regular' : 'System',
         },
         h5: {
             fontSize: 13,
@@ -101,16 +90,16 @@ export function useFonts() {
         default: {
             fontSize: 16,
             color: colors.text,
-            fontFamily: fontsLoaded ? 'Inter_400Regular' : 'System',
+            fontFamily: areFontsLoaded ? 'Inter_400Regular' : 'System',
         },
         description: {
             color: colors.text,
             fontSize: 14,
-            fontFamily: fontsLoaded ? 'Inter_400Regular' : 'System',
+            fontFamily: areFontsLoaded ? 'Inter_400Regular' : 'System',
         },
         quote: {
             color: colors.text,
-            fontFamily: fontsLoaded ? 'Inter_400Regular' : 'System',
+            fontFamily: areFontsLoaded ? 'Inter_400Regular' : 'System',
             fontSize: 16,
             fontStyle: 'italic',
         },
@@ -136,7 +125,7 @@ export function useFonts() {
         },
         alert: {
             color: colors.text,
-            fontFamily: fontsLoaded ? 'Inter_500Medium' : 'System',
+            fontFamily: areFontsLoaded ? 'Inter_500Medium' : 'System',
             fontSize: 16,
             fontWeight: 'bold',
         },
@@ -154,13 +143,13 @@ export function useFonts() {
             fontSize: 18,
             fontWeight: 'bold',
             color: colors.text,
-            fontFamily: fontsLoaded ? 'Inter_600SemiBold' : 'System',
+            fontFamily: areFontsLoaded ? 'Inter_600SemiBold' : 'System',
         },
         courseSubTitle: {
             fontSize: 12,
             fontStyle: 'italic',
             color: colors.subTitle,
-            fontFamily: fontsLoaded ? 'Inter_400Regular' : 'System',
+            fontFamily: areFontsLoaded ? 'Inter_400Regular' : 'System',
         },
     };
 
