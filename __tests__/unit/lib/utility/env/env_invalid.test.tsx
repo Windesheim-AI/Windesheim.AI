ad78d0c3fdf6ebec5226d4e2903ed93900ad5bc3
Yannick Bruintjes <yannickbruintjes@hotmail.com>
2023-11-21 11:50:26 +0100

    test: :white_check_mark: changed invalid tests

--- __tests__/unit/lib/utility/env/env_invalid.test.tsx
+++ __tests__/unit/lib/utility/env/env_invalid.test.tsx
@@ -1,24 +1,13 @@
 import { getEnvValue } from '../../../../../src/lib/utility/env/env';
 import { EnvOptions } from '../../../../../src/lib/utility/env/env.values';
 
-// Mock the ./env.values module
-jest.mock('../../../../../src/lib/utility/env/env.values.ts', () => ({
-    EnvValues: {
-        GoogleTranslateApiKey: undefined,
-        OpenAIApiKey: 'mockOpenAIApiKey',
-        AiEnabled: 'true',
-        WordPressUsername: 'mockUsername',
-        WordPressPassword: 'mockPassword',
-    },
-}));
-
 describe('getEnvValue when undefined', () => {
     it('should throw error for missing or invalid environment variable', () => {
         expect(() =>
-            getEnvValue(EnvOptions.GoogleTranslateApiKey, true),
+            getEnvValue('ThisDoesNotExist' as EnvOptions, true),
         ).toThrowError(
             "Couldn't find or invalid environment variable: " +
-                EnvOptions.GoogleTranslateApiKey,
+                'ThisDoesNotExist',
         );
     });
 });
