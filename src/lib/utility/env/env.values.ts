2c0714579260a7e1e753c3b86bad5864f47d02f3
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-11-22 13:21:49 +0100

    chore: :construction: Remove unused env value

--- src/lib/utility/env/env.values.ts
+++ src/lib/utility/env/env.values.ts
@@ -10,7 +10,6 @@ import {
 } from '@env';
 
 export enum EnvOptions {
-    GoogleTranslateApiKey = 'GoogleTranslateApiKey',
     OpenAIApiKey = 'OpenAIApiKey',
     AiEnabled = 'AiEnabled',
     WordPressUsername = 'WordPressUsername',
@@ -20,7 +19,6 @@ export enum EnvOptions {
 export type Env = Record<EnvOptions, string | undefined>;
 
 export const mockEnvValues: Env = {
-    [EnvOptions.GoogleTranslateApiKey]: 'mockGoogleTranslateApiKey',
     [EnvOptions.OpenAIApiKey]: 'mockOpenAIApiKey',
     [EnvOptions.AiEnabled]: 'true',
     [EnvOptions.WordPressUsername]: 'mockUsername',
@@ -28,7 +26,6 @@ export const mockEnvValues: Env = {
 };
 
 export const EnvValues: Env = {
-    [EnvOptions.GoogleTranslateApiKey]: EXPO_PUBLIC_GTR_API_KEY,
     [EnvOptions.OpenAIApiKey]: OPENAI_API_KEY,
     [EnvOptions.AiEnabled]: AI_ENABLED,
     [EnvOptions.WordPressUsername]: WP_USERNAME,
