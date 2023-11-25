5e34225cb89d6338e835fbce35d28489a2bfa964
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-11-25 16:47:43 +0100

    chore: :construction: Update data wrapper to not throw errors

--- src/components/general/base/DataWrapper.tsx
+++ src/components/general/base/DataWrapper.tsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
+import { useFonts } from '../../../constants/Fonts';
 import LoadingScreen from '../../loadingscreen/LoadingScreen';
+import { TextTranslated } from '../text/TextTranslated';
+import { PageView } from '../views/PageView';
 
-export type DataWrapperProps = {
+export type Props = {
     error?: Error | undefined;
     isLoading?: boolean;
-    children: React.ReactNode;
+    children: ReactNode;
 };
 
 // This component is used to wrap data that is being fetched from the backend.
 // It will show a loading screen while the data is being fetched.
-export function DataWrapper({ error, isLoading, children }: DataWrapperProps) {
+export function DataWrapper({ error, isLoading, children }: Props) {
+    const fonts = useFonts();
+
     if (isLoading) {
         return <LoadingScreen />;
     }
 
     if (error) {
-        throw error;
+        return (
+            <PageView>
+                <TextTranslated
+                    style={fonts.h1}
+                    text="An error occurred while loading the data"
+                />
+            </PageView>
+        );
     }
 
     return children;
