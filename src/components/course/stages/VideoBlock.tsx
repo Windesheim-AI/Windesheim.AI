1ee2fb32c189c26b0009d36780833ca3134220a0
Yannick Bruintjes <yannickbruintjes@hotmail.com>
2023-11-28 12:18:15 +0100

    feat: :sparkles: added image and YT support

--- src/components/course/stages/VideoBlock.tsx
+++ src/components/course/stages/VideoBlock.tsx
@@ -1,9 +1,10 @@
-import { ResizeMode, Video } from 'expo-av';
+import { Audio, ResizeMode, Video } from 'expo-av';
 import React from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import { Button, Dimensions, StyleSheet, View } from 'react-native';
 
 import BlockWrapper from './block';
 import { VideoOptions } from '../../../types/CourseStageBlock';
+import YoutubePlayer from "react-native-youtube-iframe";
 
 export function VideoBlock({ options }: { options: VideoOptions }) {
     const video = React.useRef(null);
@@ -15,35 +16,66 @@ export function VideoBlock({ options }: { options: VideoOptions }) {
             flex: 1, // You might want to adjust this flex value or set fixed width/height
             justifyContent: 'center',
             alignItems: 'center',
+            marginBottom: 20,
         },
         video: {
             // If you know the aspect ratio of your video, set it here
             aspectRatio: 16 / 9, // Assuming a 16:9 aspect ratio
             width: '100%', // Make the video width stretch to the container width
             maxHeight: '100%', // Optional: if you want to set a max height
-            marginBottom: 20,
         },
+        ytContainer: {
+            marginRight: 10,
+            marginLeft: 10,
+        }
     });
+
+    // enable ios audio when in silent mode
+    Audio.setAudioModeAsync({
+        playsInSilentModeIOS: true,
+    });
+
+    // get the YT id from the url if it's a YT url
+    const getYTId = (url: string) => {
+        const regex = /(?:\?v=|&v=|youtu\.be\/)(.*?)(?:\?|&|$)/;
+        const match = url.match(regex);
+        return match && match[1].length === 11 ? match[1] : false;
+    }
 
     return (
         <BlockWrapper style={styles.container}>
-            <Video
-                style={styles.video}
-                ref={video}
-                source={{
-                    uri: options.videoURL,
-                }}
-                useNativeControls
-                resizeMode={ResizeMode.COVER}
-                isLooping
-                onPlaybackStatusUpdate={status => setStatus(() => status)}
-            />
-            <Button
-                title={status.isPlaying ? 'Pause' : 'Play'}
-                onPress={() =>
-                    status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
-                }
-            />
+            {!options.videoURL.includes('youtube' || 'yt') ? (
+                <>
+                    <Video
+                        style={styles.video}
+                        ref={video}
+                        source={{
+                            uri: options.videoURL,
+                        }}
+                        resizeMode={ResizeMode.COVER}
+                        isLooping
+                        onPlaybackStatusUpdate={status => setStatus(() => status)}
+                    />
+                    <Button
+                        //@ts-ignore
+                        title={status.isPlaying ? 'Pause' : 'Play'}
+                        onPress={() =>
+                            //@ts-ignore
+                            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+                        }
+                    />
+                </>
+            ) : (
+
+                <View style={styles.ytContainer}>
+                    <YoutubePlayer
+                        height={Dimensions.get('window').height / 3}
+                        width={Dimensions.get('window').width - 90}
+                        play={true}
+                        videoId={getYTId(options.videoURL) || ''}
+                    />
+                </View>
+            )}
         </BlockWrapper>
     );
 }
