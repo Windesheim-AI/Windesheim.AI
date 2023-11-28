c769fe67b8a35788ea855291a74a3cfea24eab37
LeeMineo <lms4882@gmail.com>
2023-11-28 10:19:17 +0100

    Update logo

--- src/lib/utility/env/env.ts
+++ src/lib/utility/env/env.ts
@@ -3,8 +3,12 @@ import { EnvOptions, EnvValues, mockEnvValues } from './env.values';
 export function getEnvValue(key: EnvOptions, mock = false): string {
     const value = mock ? mockEnvValues[key] : EnvValues[key];
     if (value === undefined) {
+        let str = '';
+        for (const envKey in EnvValues) {
+            str += `\n${envKey}: ${EnvValues[envKey]}`;
+        }
         throw new Error(
-            `Couldn't find or invalid environment variable: ${key}`,
+            `Couldn't find or invalid environment variable: ${key} current env values ${str}`,
         );
     }
     return value;
