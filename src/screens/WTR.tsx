cac07d5b6bf89022583cf4fd06995b1a142164ac
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-12-08 11:15:17 +0100

    chore: :construction: Improve namespacing of components and screens

--- src/screens/WTR.tsx
+++ src/screens/WTR.tsx
@@ -1,12 +1,12 @@
 import { useRoute } from '@react-navigation/native';
 import React from 'react';
 
-import { WTRSContentScreenProps } from './WTRContent';
-import { TechProviders } from '../../components/WTR/TechProviders';
-import { Themes } from '../../components/WTR/Theme';
-import { WTRContentDisplay } from '../../components/WTR/WTRContentDisplay';
-import { PageView } from '../../components/general/views/PageView';
-import { WhScrollView } from '../../components/general/views/WhScrollView';
+import { WTRSContentScreenProps } from './WTR/WTRContent';
+import { TechProviders } from '../components/WTR/TechProviders';
+import { Themes } from '../components/WTR/Theme';
+import { WTRContentDisplay } from '../components/WTR/WTRContentDisplay';
+import { PageView } from '../components/general/views/PageView';
+import { WhScrollView } from '../components/general/views/WhScrollView';
 
 export const WTRScreen = () => {
     const route = useRoute();
