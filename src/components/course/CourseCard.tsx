1294d1037d7037e73f13637498317db0d040725d
Yannick Bruintjes <yannickbruintjes@hotmail.com>
2023-12-12 14:39:40 +0100

    style: :art: changed ui for course card

---
+++ src/components/course/CourseCard.tsx
@@ -0,0 +1,167 @@
+import React from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+import { Bar } from 'react-native-progress';
+
+import {
+    shadow,
+    useColorConfig,
+    useColorStateConfig,
+} from '../../lib/constants/Colors';
+import { useFonts } from '../../lib/constants/Fonts';
+import { Card } from '../general/base/Card';
+import { IconLine } from '../general/base/IconLine';
+import { CheckMarkFlag } from '../general/base/checkFlagMark';
+import { TextTranslated } from '../general/text/TextTranslated';
+import { IntractableView } from '../general/views/IntractableView';
+
+type Props = {
+    title: string | undefined;
+    completedTasks: number;
+    totalTasks: number;
+    level: string;
+    likes: number;
+    onPress: () => void;
+};
+
+export function CourseCard({
+    title,
+    completedTasks,
+    totalTasks,
+    level,
+    likes,
+    onPress,
+}: Props) {
+    const colors = useColorConfig();
+    const fonts = useFonts();
+    const stateColors = useColorStateConfig();
+
+    const styles = StyleSheet.create({
+        card: {
+            padding: 0,
+            flexDirection: 'row',
+        },
+        titleSide: {
+            width: '70%',
+        },
+        infoSide: {
+            padding: 10,
+            width: '30%',
+            backgroundColor: colors.subCard,
+            height: 'auto',
+            borderTopRightRadius: 10,
+            borderBottomRightRadius: 10,
+        },
+        image: {
+            width: '100%',
+            height: 100,
+            borderTopLeftRadius: 10,
+        },
+        title: {
+            ...fonts.courseTitle,
+            // warp title
+            flexWrap: 'wrap',
+        },
+        titleContainer: {
+            height: 'auto',
+            margin: 10,
+        },
+        progressBar: {
+            height: 7,
+            borderColor: colors.listItemBg,
+            backgroundColor: colors.subCard,
+            marginTop: 5,
+            borderRadius: 10,
+            ...shadow,
+        },
+        progressBarContainer: {
+            flexDirection: 'row',
+            marginRight: 30,
+        },
+        courseNIndiciator: {
+            ...fonts.description,
+            marginLeft: 5,
+        },
+        infoMarginContainer: {
+            marginTop: 'auto',
+            marginBottom: 'auto',
+            alignItems: 'flex-start',
+            paddingTop: 40,
+        },
+        iconLines: {
+            marginBottom: 10,
+        },
+        button: {
+            backgroundColor: colors.bg3,
+            borderRadius: 10,
+            paddingTop: 10,
+            paddingBottom: 10,
+            paddingLeft: 20,
+            paddingRight: 20,
+            marginTop: 10,
+            ...shadow,
+            ...fonts.stageTime,
+        },
+    });
+
+    return (
+        <IntractableView onPress={onPress}>
+            <Card style={styles.card}>
+                <View style={styles.titleSide}>
+                    <Image
+                        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                        source={require('../../assets/images/bgImages/robot.png')}
+                        style={styles.image}
+                    />
+                    <View style={styles.titleContainer}>
+                        <TextTranslated style={styles.title} text={title} />
+                        <View style={styles.progressBarContainer}>
+                            <Bar
+                                progress={completedTasks / totalTasks}
+                                color={stateColors.colors.secondary[1]}
+                                style={styles.progressBar}
+                            />
+                            <TextTranslated
+                                style={styles.courseNIndiciator}
+                                text={`${completedTasks}/${totalTasks}`}
+                            />
+                        </View>
+                    </View>
+                </View>
+                <View style={styles.infoSide}>
+                    {/* star icon */}
+                    <View style={styles.infoMarginContainer}>
+                        <IconLine
+                            text={`Level ${level}`}
+                            iconName="star"
+                            iconColor={stateColors.colors.secondary[1]}
+                            textStyle={{ color: colors.text }}
+                            leftorRight="left"
+                            size={20}
+                            style={styles.iconLines}
+                        />
+                        <IconLine
+                            text={`${likes}`}
+                            iconName="heart"
+                            iconColor={stateColors.colors.danger[0]}
+                            textStyle={{ color: colors.text }}
+                            leftorRight="left"
+                            size={20}
+                        />
+                        <View style={styles.button}>
+                            <IconLine
+                                text="Start"
+                                iconName="play"
+                                iconColor={colors.white}
+                                textStyle={{ color: colors.white }}
+                                leftorRight="left"
+                                size={20}
+                            />
+                        </View>
+                    </View>
+                </View>
+
+                {true ? <CheckMarkFlag /> : null}
+            </Card>
+        </IntractableView>
+    );
+}
--- src/screens/Course/Courses.tsx
+++ src/screens/Course/Courses.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Bar } from 'react-native-progress';
 
+import { CourseCard } from '../../components/course/CourseCard';
 import { DataWrapper } from '../../components/general/base/DataWrapper';
 import { GoBackButton } from '../../components/general/buttons/GoBackButton';
 import { TextTranslated } from '../../components/general/text/TextTranslated';
-import { IntractableView } from '../../components/general/views/IntractableView';
 import { PageScrollView } from '../../components/general/views/PageScrollView';
-import {
-    shadow,
-    useColorConfig,
-    useColorStateConfig,
-} from '../../lib/constants/Colors';
 import { useFonts } from '../../lib/constants/Fonts';
 import { HapticFeedback, HapticForces } from '../../lib/haptic/Hooks';
 import { useMapMultipleCoursesToData } from '../../lib/repositories/courses/mapMultipleCourseToData';
@@ -20,49 +14,18 @@ import { useNavigation } from '../../lib/utility/navigation/useNavigation';
 import { Routes } from '../../routes/routes';
 import { CourseDataMapped } from '../../types/Course';
 
-function getProgressPercentage(course: CourseDataMapped) {
-    const stages = course.stageData;
-    if (stages.length === 0) {
-        return 0;
-    }
-
-    const stagesCompleted = stages?.filter((stage) => stage.isCompletedByUser);
-    if (stagesCompleted?.length === 0) {
-        return 0;
-    }
-
-    return stagesCompleted.length / stages.length;
+function getAmountCompletedTask(course: CourseDataMapped) {
+    if (!course.stageData) return 0;
+    return course.stageData?.map((stage) => stage.isCompletedByUser).length;
 }
 
 export function Courses() {
     const fonts = useFonts();
-    const colors = useColorConfig();
-    const colorStateConfig = useColorStateConfig();
     const { data, isLoading, error } = useAllCourses();
     const courses = useMapMultipleCoursesToData(data);
     const navigator = useNavigation();
 
     const styles = StyleSheet.create({
-        card: {
-            backgroundColor: colors.listItemBg,
-            borderRadius: 10,
-            padding: 16,
-            marginBottom: 16,
-            ...shadow,
-            elevation: 5,
-            ...colorStateConfig.highContrastBorder,
-        },
-        title: {
-            ...fonts.courseTitle,
-        },
-        description: {
-            ...fonts.description,
-            marginTop: 10,
-        },
-        progressBar: {
-            marginTop: 15,
-            ...shadow,
-        },
         cardContainer: {
             marginTop: 15,
         },
@@ -88,32 +51,15 @@ export function Courses() {
                 {/* map the courses */}
                 <View style={styles.cardContainer} testID="test-container">
                     {courses?.map((course: CourseDataMapped) => (
-                        <IntractableView
+                        <CourseCard
                             key={course.courseId}
-                            style={styles.card}
+                            title={course.title}
+                            completedTasks={getAmountCompletedTask(course)}
+                            totalTasks={course.stageData?.length ?? 0}
+                            level="0"
+                            likes={0}
                             onPress={() => onPress(course.courseId)}
-                            testID={`course-card-${course.courseId}`}
-                        >
-                            <TextTranslated
-                                style={styles.title}
-                                text={course.title ?? ''}
-                            />
-                            <TextTranslated
-                                style={styles.description}
-                                text={course.description ?? ''}
-                            />
-
-                            <View style={styles.progressBar}>
-                                <Bar
-                                    progress={getProgressPercentage(course)}
-                                    width={null}
-                                    height={7}
-                                    borderRadius={10}
-                                    color={colors.success}
-                                    unfilledColor={colors.listItemBg}
-                                />
-                            </View>
-                        </IntractableView>
+                        />
                     ))}
                 </View>
             </PageScrollView>
