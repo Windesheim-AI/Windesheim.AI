76dd04d4028195046c06debccb13274f2554085c
Koen van Meijeren <koenvanmeijeren@gmail.com>
2023-12-13 16:09:32 +0100

    chore: :construction: Remove image viewer package

--- src/components/course/stages/ImageBlock.tsx
+++ src/components/course/stages/ImageBlock.tsx
@@ -1,20 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Image, StyleSheet } from 'react-native';
 
 import BlockWrapper from './block';
-import { HapticFeedback, HapticForces } from '../../../lib/haptic/Hooks';
 import { ImageOptions } from '../../../types/CourseStageBlock';
-import { InteractiveView } from '../../general/views/InteractiveView';
-
-// Import ImageView only on mobile
-let ImageView: any;
-if (process.env.TARGET === 'mobile') {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-member-access
-    ImageView = require('react-native-image-zoom-viewer').default;
-}
 
 export function ImageBlock({ options }: { options: ImageOptions }) {
-    const [visible, setVisible] = useState(false);
     const styles = StyleSheet.create({
         container: {
             flex: 1,
@@ -24,35 +14,14 @@ export function ImageBlock({ options }: { options: ImageOptions }) {
         },
     });
 
-    function onPress() {
-        HapticFeedback(HapticForces.Light);
-        setVisible(true);
-    }
-
     return (
         <BlockWrapper style={styles.container}>
-            <InteractiveView onPress={onPress}>
-                <Image
-                    style={styles.image}
-                    source={{
-                        uri: options.imageURL,
-                    }}
-                />
-            </InteractiveView>
-            {ImageView ? (
-                <ImageView
-                    images={[
-                        {
-                            uri: options.imageURL,
-                        },
-                    ]}
-                    imageIndex={0}
-                    visible={visible}
-                    swipeToCloseEnabled
-                    doubleTapToZoomEnabled
-                    onRequestClose={() => setVisible(false)}
-                />
-            ) : null}
+            <Image
+                style={styles.image}
+                source={{
+                    uri: options.imageURL,
+                }}
+            />
         </BlockWrapper>
     );
 }
