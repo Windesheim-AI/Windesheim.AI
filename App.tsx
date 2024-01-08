f4b996f59112b5ce71c66382b18d3057704d1e8d
Tim Bentum <tim_bentum@live.nl>
2024-01-08 13:56:59 +0100

    feat: :boom: Add Sentry

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import './src/lib/translations/i18n'; // Required import to enable translations
 import ErrorBoundary from 'react-native-error-boundary';
 import { Provider } from 'react-redux';
+import './src/lib/translations/i18n'; // Required import to enable translations
 
+import * as Sentry from '@sentry/react-native';
 import ErrorFallback from './src/components/general/error/ErrorBoundary';
 import AppLoader from './src/components/loadingscreen/AppLoader';
 import WhNavigationContainer from './src/components/navigation/WhNavigationContainer';
 import SplashScreenOrApp from './src/components/splashscreen/SplashScreenOrApp';
 import { store } from './src/lib/redux/Store';
 import AppProviders from './src/providers/AppProviders';
+
+Sentry.init({
+    dsn: 'https://0c858c0fb3560e47e20b685b2dded9c9@o4506535705772032.ingest.sentry.io/4506535711080448',
+});
 
 export default function App() {
     return (
--- metro.config.js
+++ metro.config.js
@@ -5,6 +5,10 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 const { getDefaultConfig } = require('@expo/metro-config');
 
+const {
+    createSentryMetroSerializer
+} = require("@sentry/react-native/dist/js/tools/sentryMetroSerializer");
+
 const defaultConfig = getDefaultConfig(__dirname);
 const { assetExts, sourceExts } = defaultConfig.resolver;
 
@@ -18,10 +22,15 @@ const config = {
     transformer: {
         babelTransformerPath: require.resolve('react-native-svg-transformer'),
     },
+
     resolver: {
         assetExts: assetExts.filter((ext) => ext !== 'svg'),
         sourceExts: [...sourceExts, 'svg'],
     },
+
+    serializer: {
+        customSerializer: createSentryMetroSerializer()
+    }
 };
 
 // eslint-disable-next-line no-shadow
