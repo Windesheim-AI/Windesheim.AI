8fdd4811b5cb49571fa5884537534b7bddd3c7f3
Pierre-Louis LOPEZ <pilou@MacBook-Pro-de-Pierre-Louis.local>
2024-04-04 15:29:16 +0200

    settings button without the icone

--- src/components/general/buttons/SettingButton.tsx
+++ src/components/general/buttons/SettingButton.tsx
@@ -1,110 +1,18 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { Pressable, StyleSheet, View } from 'react-native';
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import { Button } from 'react-native';
 
-import { useColorConfig } from '../../../lib/constants/Colors';
-import { useFonts } from '../../../lib/constants/Fonts';
-import { HapticFeedback, HapticForces } from '../../../lib/haptic/Hooks';
-import { useNavigation } from '../../../lib/utility/navigation/useNavigation';
-import { truncate } from '../../../lib/utility/stringutils';
-import { TextTranslated } from '../text/TextTranslated';
+import { Routes } from '../../../routes/routes';
 
-export type SettingButtonProps = {
-    onPress?: () => void;
-    title: string;
-    description: string;
-    icon: string;
-    screenName?: string;
-};
-
-export const SettingButton = ({
-    title,
-    description,
-    onPress,
-    icon,
-    screenName,
-}: SettingButtonProps) => {
-    const colors = useColorConfig();
+export const DisclaimerCard = () => {
     const navigation = useNavigation();
-    const fonts = useFonts();
 
-    if (!onPress) {
-        /* istanbul ignore next */
-        if (!screenName) {
-            throw new Error(
-                'SettingButton requires either onPress or screenName to be defined',
-            );
-        }
-
-        /* istanbul ignore next */
-        onPress = () => {
-            HapticFeedback(HapticForces.Light);
-            navigation.navigate(screenName);
-        };
-    }
-
-    const styles = StyleSheet.create({
-        buttonContainer: {
-            flexDirection: 'row',
-            alignItems: 'center',
-            borderColor: colors.gray,
-            borderWidth: 1,
-            padding: 10,
-            borderRadius: 10,
-            marginBottom: 20,
-        },
-        iconContainer: {
-            marginRight: 10,
-        },
-        icon: {
-            ...fonts.icon,
-            color: colors.text,
-        },
-        titleContainer: {
-            flex: 1,
-            marginLeft: 10,
-            marginRight: 10,
-        },
-        title: {
-            ...fonts.button,
-            marginBottom: 5,
-            color: colors.text,
-        },
-        description: {
-            ...fonts.description,
-        },
-        arrowContainer: {
-            justifyContent: 'center',
-        },
-        arrow: {
-            width: 20,
-            height: 20,
-            color: colors.text,
-        },
-    });
     return (
-        <Pressable
-            testID={title}
-            style={styles.buttonContainer}
-            onPress={onPress}
-        >
-            <View style={styles.iconContainer}>
-                <FontAwesome5 style={styles.icon} name={icon} size={24} />
-            </View>
-            <View style={styles.titleContainer}>
-                <TextTranslated style={styles.title} text={title} />
-                <TextTranslated
-                    style={styles.description}
-                    text={truncate(description, 50)}
-                />
-            </View>
-            <View style={styles.arrowContainer}>
-                <FontAwesome5
-                    style={styles.arrow}
-                    name="chevron-right"
-                    size={24}
-                />
-            </View>
-        </Pressable>
+        <Button
+            title="Settings"
+            onPress={() => {
+                navigation.navigate(Routes.Settings as never);
+            }}
+        />
     );
 };
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,29 +1,46 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Button, View, StyleSheet } from 'react-native';
 
 import { TechProviders } from '../components/WTR/TechProviders';
 import { Themes } from '../components/WTR/Themes';
 import { TitleWithSeeAll } from '../components/general/text/TitleWithSeeAll';
 import { PageScrollView } from '../components/general/views/PageScrollView';
 import { PromptsLimitedView } from '../components/promptLibary/PromptsLimitedView';
+import { useNavigation } from '../lib/utility/navigation/useNavigation';
 import { Routes } from '../routes/routes';
 
 export const HomeScreen = () => {
+    const navigation = useNavigation();
     const description =
         'Artificial intelligence is the key to innovating the future and transforming our lives';
 
     return (
-        <PageScrollView title="Home" description={description}>
-            <View>
-                <TitleWithSeeAll
-                    title="Useful Prompts"
-                    navigateToRoute={Routes.PromptLibrary}
-                />
-                <PromptsLimitedView limit={3} />
-            </View>
+        <View style={styles.container}>
+            <Button
+                title="Settings"
+                onPress={() => {
+                    navigation.navigate(Routes.Settings);
+                }}
+            />
+            <PageScrollView title="Home" description={description}>
+                <View>
+                    <TitleWithSeeAll
+                        title="Useful Prompts"
+                        navigateToRoute={Routes.PromptLibrary}
+                    />
+                    <PromptsLimitedView limit={3} />
+                </View>
 
-            <Themes limit={3} />
-            <TechProviders limit={3} />
-        </PageScrollView>
+                <Themes limit={3} />
+                <TechProviders limit={3} />
+            </PageScrollView>
+        </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'flex-end',
+    },
+});
