4098c76a3b3681c7f558bddee94e18c7fa12b936
giz3m <s1173438@student.windesheim.nl>
2024-04-04 14:02:26 +0200

    updated some errors for testing

--- src/components/general/card/DisclaimerCard.tsx
+++ src/components/general/card/DisclaimerCard.tsx
@@ -62,6 +62,7 @@ export const DisclaimerCard = () => {
                 <Image
                     style={styles.exclamationMark}
                     source={
+                        // eslint-disable-next-line @typescript-eslint/no-var-requires
                         require('../../../assets/images/Icon/exclamation-mark.png') as ImageSourcePropType
                     }
                 />
