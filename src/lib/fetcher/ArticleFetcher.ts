9afc312756262a9e37603492f91edc4593f4c4ea
s1155501 <74300271+s1155501@users.noreply.github.com>
2024-04-16 10:30:21 +0200

    added quick articlefetcher mock function

---
+++ src/lib/fetcher/ArticleFetcher.ts
@@ -0,0 +1,24 @@
+// Please note: this is a mock return for demo purposes only
+// TODO: Implement the actual fetcher logic
+export const getArticleArray = () => {
+    return [
+        {
+            title: 'Article 1',
+            description: 'This is the article 1 description',
+            url: 'https://www.example.com/',
+            image: 'https://placehold.co/400x400/000000/FFFFFF/png',
+        },
+        {
+            title: 'Article 2',
+            description: 'This is the article 2 description',
+            url: 'https://www.example.com/',
+            image: 'https://placehold.co/400x400/000000/FFFFFF/png',
+        },
+        {
+            title: 'Article 3',
+            description: 'This is the article 3 description',
+            url: 'https://www.example.com/',
+            image: 'https://placehold.co/400x400/000000/FFFFFF/png',
+        },
+    ];
+};
