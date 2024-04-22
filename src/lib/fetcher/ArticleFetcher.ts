60e91f72e1dbfa5bf7c5347ccdd667bf2045b3d5
s1155501 <74300271+s1155501@users.noreply.github.com>
2024-04-22 12:49:16 +0200

    Updated mocking article funcion to more acurately reflect real data

--- src/lib/fetcher/ArticleFetcher.ts
+++ src/lib/fetcher/ArticleFetcher.ts
@@ -9,21 +9,21 @@ export const getArticleArray = () => {
             description:
                 'Het gebruik van AI roept meerdere ethische vraagstukken op, over onder andere privacy, aansprakelijkheid en gelijkwaardigheid. Hoewel er vee...',
             url: 'https://www.example.com/',
-            image: 'https://placehold.co/400x400/000000/FFFFFF/png',
+            image: 'https://placehold.co/500x400/000000/FFFFFF/png',
             catagory: ['Ethical', ' Social'],
         },
         {
             title: 'Article 2',
             description: 'This is the article 2 description',
             url: 'https://www.example.com/',
-            image: 'https://placehold.co/400x400/000000/FFFFFF/png',
+            image: 'https://placehold.co/400x600/000000/FFFFFF/png',
             catagory: ['Ethical'],
         },
         {
             title: 'Article 3',
             description: 'This is the article 3 description',
             url: 'https://www.example.com/',
-            image: 'https://placehold.co/400x400/000000/FFFFFF/png',
+            image: 'https://placehold.co/200x200/000000/FFFFFF/png',
             catagory: ['Ethical', 'Legal', 'Social', 'Technical'],
         },
     ];
