0bbbded6c0293be5bcb85f9fe5990b9a9b77ba2a
s1155501 <74300271+s1155501@users.noreply.github.com>
2024-04-24 14:04:00 +0200

    Chore: added temporary introduction texts to the articles page.

--- src/screens/Articles.tsx
+++ src/screens/Articles.tsx
@@ -12,14 +12,14 @@ export function Articles() {
             <View>
                 <TitleSimple
                     titleText="Podcasts"
-                    explainationText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                    explainationText="Here you will soon find the newest episodes of the official Windesheim.AI podcast."
                 />
-                <TextTranslated text="Soon TM" />
+                <TextTranslated text="loading..." />
             </View>
             <View>
                 <TitleSimple
                     titleText="Articles"
-                    explainationText="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                    explainationText="Here you will find a collection of engaging articles highlighting the latest developments, trends and insights in the field of AI. Our experts have carefully selected these articles to give you a deeper insight into the world of AI. With each article on our webpage, we also tag the relevant ELSA categories."
                 />
                 <ArticleLimitedView />
             </View>
