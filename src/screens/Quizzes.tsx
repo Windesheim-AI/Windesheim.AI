21b811b195936f70e9da16a562dbae9f55af768f
s1155501 <74300271+s1155501@users.noreply.github.com>
2024-04-25 14:15:21 +0200

    Fix: added a sample quizzes page to test design changes on

--- src/screens/Quizzes.tsx
+++ src/screens/Quizzes.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View } from 'react-native';
+
+import { CoursesOverview } from '../components/course/CoursesOverview';
+import { TextTranslated } from '../components/general/text/TextTranslated';
+import { TitleSimple } from '../components/general/text/TitleSimple';
+import { PageScrollView } from '../components/general/views/PageScrollView';
 
 export function Quizzes() {
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Courses + Quizzes</Text>
-            <Text style={styles.placeholderText}>NO CONTENT FOR NOW!</Text>
-        </View>
+        <PageScrollView>
+            <View>
+                <TitleSimple
+                    titleText="Courses"
+                    explainationText="Here you will soon find the newest interactive courses to learn about Generative AI."
+                />
+                <CoursesOverview />
+            </View>
+            <View>
+                <TitleSimple
+                    titleText="Quizzes"
+                    explainationText="Here you will soon find the newest quizzes to learn in a fun and interactive way about Generative AI."
+                />
+                <TextTranslated text="Loading..." />
+            </View>
+        </PageScrollView>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 20,
-    },
-    placeholderText: {
-        fontSize: 18,
-        textAlign: 'center',
-    },
-});
