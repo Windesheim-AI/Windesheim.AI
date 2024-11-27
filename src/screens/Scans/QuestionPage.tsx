9a6d9f38beea22606032e3f61e8cef01cbf8be94
Luximus <thomaslipman10@gmail.com>
2024-11-27 11:33:14 +0100

    chore: ✏️ comments and refactoring

--- src/screens/Scans/QuestionPage.tsx
+++ src/screens/Scans/QuestionPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
 import { Button, ProgressBar } from 'react-native-paper';
 
@@ -7,6 +7,10 @@ import useScanQuestionRange from '../../lib/repositories/scans/useScanQuestionRa
 import useSingleScan from '../../lib/repositories/scans/useSingleScan';
 import { ScanQuestion } from '../../types/Scan';
 
+/**
+ * The QuestionPage component displays a series of questions with navigation buttons.
+ * @returns {JSX.Element} - The rendered component.
+ */
 export function QuestionPage() {
     const [currentQuestionIndex, setCurrentQuestionIndex] =
         React.useState<number>(0);
@@ -30,10 +34,14 @@ export function QuestionPage() {
     const parsedQuestions: ScanQuestion[] = [];
 
     if (scanData && parsedQuestions.length === 0) {
+        // Sort the categories by index.
         scanData.categories
+            .slice()
             .sort((a, b) => parseInt(a.idx, 10) - parseInt(b.idx, 10))
             .forEach((category) => {
+                // Sort the questions by index.
                 category.questions
+                    .slice()
                     .sort((a, b) => parseInt(a.idx, 10) - parseInt(b.idx, 10))
                     .forEach((question) => {
                         parsedQuestions.push(question);
@@ -41,55 +49,50 @@ export function QuestionPage() {
             });
     }
 
-    const fetchQuestions = (currentId: string) => {
+    /**
+     * Fetches questions based on the current question ID.
+     * @param {string} currentId - The ID of the current question.
+     */
+    const fetchQuestions = (currentId: string): void => {
         const currentIndex = parsedQuestions.findIndex(
             (q) => q.id === currentId,
         );
 
-        const indexes = [
-            currentIndex - 1,
-            currentIndex,
-            currentIndex + 1,
-            currentIndex + 2,
-            currentIndex + 3,
-            currentIndex + 4,
-        ];
+        // Creates an array with the indexes of the questions to fetch.
+        // The starting index is the current index minus 1.
+        const indexes = Array.from(
+            { length: 6 },
+            (_, i) => currentIndex - 1 + i,
+        ).filter((index) => index >= 0 && index < parsedQuestions.length);
 
-        const ids: string[] = [];
+        // Creates an array with the IDs of the questions to fetch, based on the indexes.
+        const ids = indexes
+            .map((index) => parsedQuestions[index]?.id)
+            .filter((id) => id && !questionCache.some((q) => q.id === id));
 
-        indexes.forEach((index) => {
-            if (parsedQuestions.length === 0) {
-                if (index >= 0) {
-                    ids.push(index.toString());
-                }
-                return;
-            }
-
-            const question = parsedQuestions.find(
-                (q) => q.id === index.toString(),
-            );
-
-            if (question && !questionCache.find((q) => q.id === question.id)) {
-                ids.push(question.id);
-            }
-        });
-
+        // If there are no IDs to fetch, return.
         if (ids.length === 0) return;
 
+        // FetchScanQuestionRange is a function that fetches questions from the API.
         fetchScanQuestionRange(ids)
             .then((data: ScanQuestion[]) => {
-                if (!data) return;
-
-                const newQuestionCache = [...questionCache];
-                data.forEach((q) => {
-                    if (!newQuestionCache.find((qc) => qc.id === q.id)) {
-                        newQuestionCache.push(q);
-                    }
-                });
-                setQuestionCache(newQuestionCache);
-
-                if (currentQuestion === undefined) {
-                    setCurrentQuestion(data.find((q) => q.id === currentId));
+                if (data) {
+                    // Adds the fetched questions to the question cache.
+                    setQuestionCache((prevCache) => [
+                        ...prevCache,
+                        ...data
+                            // Filter out questions that are already in the cache.
+                            .filter(
+                                (q) => !prevCache.some((qc) => qc.id === q.id),
+                            )
+                            // Add the category ID to the question.
+                            .map((q) => ({
+                                ...q,
+                                categoryId: parsedQuestions.find(
+                                    (pq) => pq.id === q.id,
+                                )?.categoryId,
+                            })),
+                    ]);
                 }
             })
             .catch((error) => {
@@ -115,52 +118,74 @@ export function QuestionPage() {
         setCurrentQuestionIndex(parseInt(parsedQuestions[0].id, 10));
     }
 
-    const showNextQuestion = () => {
+    /**
+     * Displays the next question.
+     */
+    const showNextQuestion = (): void => {
         const nextIndex = currentQuestionIndex + 1;
         showQuestion(nextIndex);
     };
 
-    const showPreviousQuestion = () => {
+    /**
+     * Displays the previous question.
+     */
+    const showPreviousQuestion = (): void => {
         const previousIndex = currentQuestionIndex - 1;
         showQuestion(previousIndex);
     };
 
-    const showQuestion = (index: number) => {
+    /**
+     * Displays a question based on the given index. Used by showNextQuestion and showPreviousQuestion.
+     * @param {number} index - The index of the question to display.
+     */
+    const showQuestion = (index: number): void => {
+        // Clamp the index to the range of the questions.
         const maxIndex = parsedQuestions.length;
+        index = Math.max(1, Math.min(index, maxIndex));
 
-        if (index < 1) {
-            index = 1;
-        }
-
-        if (index > maxIndex) {
-            index = maxIndex;
-        }
-
+        // Set the current question index and question.
         setCurrentQuestionIndex(index);
         setCurrentQuestion(
             questionCache.find((q) => q.id === index.toString()),
         );
 
+        // Check if the question is in the question list.
         if (!parsedQuestions[index]) return;
 
-        try {
-            fetchQuestions(parsedQuestions[index].id);
-        } catch (error) {
-            console.log(error);
-            fetchQuestions(parsedQuestions[index].id);
-        }
+        // Fetch the questions based on the current question.
+        fetchQuestions(parsedQuestions[index].id);
     };
 
-    const getProgress = () => {
+    /**
+     * Calculates the progress of the current question.
+     * @returns {number} fraction - The progress as a fraction ranging from 0 to 1.
+     */
+    const getProgress = (): number => {
         const maxIndex = parsedQuestions.length;
         const currentIndex = currentQuestionIndex;
 
         return currentIndex / maxIndex;
     };
 
-    return (
+    // Fetch the next question when the current question changes to undefined.
+    // This happens when the current question is not in the question cache.
+    useEffect(() => {
+        if (!currentQuestion && questionCache.length > 0) {
+            const question = questionCache.find(
+                (q) => q.id === currentQuestionIndex.toString(),
+            );
+            if (question) {
+                setCurrentQuestion(question);
+            }
+        }
+    }, [currentQuestion, questionCache, currentQuestionIndex]);
+
+    return currentQuestion ? (
         <View>
             <ProgressBar progress={getProgress()} />
+            <Text>ID: {currentQuestion?.id}</Text>
+            <Text>CategoryID: {currentQuestion?.categoryId}</Text>
+            <Text>Index: {currentQuestion?.idx}</Text>
             <Text>Question: {currentQuestion?.text}</Text>
             <Text>Description: {currentQuestion?.description}</Text>
             <Button onPress={showPreviousQuestion}>
@@ -169,6 +194,10 @@ export function QuestionPage() {
             <Button onPress={showNextQuestion}>
                 <Text>Next</Text>
             </Button>
+        </View>
+    ) : (
+        <View>
+            <Text>Loading the next question for you...</Text>
         </View>
     );
 }
